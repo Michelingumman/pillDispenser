a50dc8e28fe008dbe5ca6cdeae0690154267cbe6
Michelingumman <adam.michelin01@gmail.com>
2025-03-03 22:17:35 +0100

    Initial commit

---
+++ include/secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+#define WIFI_SSID = "Michaelin";
+#define WIFI_PASS = "Michaelin";
+
+
+#endif  // SECRETS_H
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "images.h"
+#include "secrets.h"
+
+
+
+
+
+void setup() {
+
+}
+
+void loop() {
+}
+
